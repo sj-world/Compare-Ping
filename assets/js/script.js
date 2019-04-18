888dd5ea3df846d125a22c2626600a4a740f3c36
sj-world <shobhitjain.sj.2403@gmail.com>
2019-04-19 04:27:36 +0530

    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,13 @@ const menuBtn = document.querySelector('.menu');
 
 menuBtn.onclick = function() {
     console.log('ok')
+
     openMenu();
 }
 
 var openMenu = function() {
     console.log('fine');
+
+    document.getElementById("myDropdown").classList.toggle("show");
 }
 
