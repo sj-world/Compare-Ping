9876cbfdfe85fbc48d2a12d64d6cbf75fbf1da23
sj-world <shobhitjain.sj.2403@gmail.com>
2019-04-19 02:44:12 +0530

    ok

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const menuBtn = document.querySelector('.menu');
+
+menuBtn.onclick = function() {
+    console.log('ok')
+    openMenu();
+}
+
+var openMenu = function() {
+    console.log('fine');
+}
+
