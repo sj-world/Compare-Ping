911b7d2bf05a39a79a671453c655e61c28558ff7
sj-world <shobhitjain.sj.2403@gmail.com>
2019-04-19 10:21:50 +0530

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,36 @@
-const menuBtn = document.querySelector('.menu');
-
-menuBtn.onclick = function() {
-    console.log('ok')
-
-    openMenu();
-}
-
-var openMenu = function() {
-    console.log('fine');
-
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
+$( function() {
+	
+	//On Scroll Functionality
+	$(window).scroll( function() {
+		var windowTop = $(window).scrollTop();
+		windowTop > 100 ? $('nav').addClass('navShadow') : $('nav').removeClass('navShadow');
+		windowTop > 100 ? $('ul').css('top','100px') : $('ul').css('top','160px');
+	});
+	
+	//Click Logo To Scroll To Top
+	$('#logo').on('click', function() {
+		$('html,body').animate({
+			scrollTop: 0
+		},500);
+	});
+	
+	//Smooth Scrolling Using Navigation Menu
+	$('a[href*="#"]').on('click', function(e){
+		$('html,body').animate({
+			scrollTop: $($(this).attr('href')).offset().top - 100
+		},500);
+		e.preventDefault();
+	});
+	
+	//Toggle Menu
+	$('#menu-toggle').on('click', function() {
+		$('#menu-toggle').toggleClass('closeMenu');
+		$('ul').toggleClass('showMenu');
+		
+		$('li').on('click', function() {
+			$('ul').removeClass('showMenu');
+			$('#menu-toggle').removeClass('closeMenu');
+		});
+	});
+	
+});
