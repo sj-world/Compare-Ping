2b25de540766da36e15a6a6c04c405b43c25f613
sj-world <shobhitjain.sj.2403@gmail.com>
2019-04-19 20:12:45 +0530

    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ $( function() {
 	//On Scroll Functionality
 	$(window).scroll( function() {
 		var windowTop = $(window).scrollTop();
-		windowTop > 100 ? $('nav').addClass('navShadow') : $('nav').removeClass('navShadow');
+		// windowTop > 100 ? $('nav').addClass('navShadow') : $('nav').removeClass('navShadow');
 		windowTop > 100 ? $('ul').css('top','100px') : $('ul').css('top','160px');
 	});
 	
@@ -17,20 +17,56 @@ $( function() {
 	//Smooth Scrolling Using Navigation Menu
 	$('a[href*="#"]').on('click', function(e){
 		$('html,body').animate({
-			scrollTop: $($(this).attr('href')).offset().top - 100
+			scrollTop: $($(this).attr('href')).offset().top - 160
 		},500);
 		e.preventDefault();
 	});
 	
 	//Toggle Menu
-	$('#menu-toggle').on('click', function() {
-		$('#menu-toggle').toggleClass('closeMenu');
-		$('ul').toggleClass('showMenu');
+	$('.menu-toggle').on('click', function() {
+		$('.menu-toggle').toggleClass('closeMenu');
+		$('.menu-box').toggleClass('showMenu');
+		// $('body').toggleClass('overflow-y');
 		
 		$('li').on('click', function() {
-			$('ul').removeClass('showMenu');
-			$('#menu-toggle').removeClass('closeMenu');
+			$('.menu-box').removeClass('showMenu');
+			$('.menu-toggle').removeClass('closeMenu');
 		});
 	});
 	
 });
+
+// menu button text toggle
+
+const menuText = document.querySelector('.menu-toggle');
+const mobileMenuIcon = document.querySelector('.mobile-menu');
+const linkClicked = document.querySelectorAll('.link-click');
+
+function checkMenuText() {
+	if(menuText.innerHTML === 'Product Menu') {
+		menuText.innerHTML = 'Close Menu';
+	} 
+	else {
+		menuText.innerHTML = 'Product Menu'
+	}
+}
+
+menuText.addEventListener('click', function() {
+	checkMenuText()
+})
+
+mobileMenuIcon.addEventListener('click', function(){
+	checkMenuText()
+})
+
+
+for(var i = 0; i<linkClicked.length; i++) {
+
+	linkClicked[i].addEventListener('click', function(){
+		checkMenuText()
+	})
+}
+
+
+
+//  menu button text toggle end
